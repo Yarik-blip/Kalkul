33e7fde11b14dfbbaf6ffb379a9554e30434ded9
Yarik-blip <75161139+Yarik-blip@users.noreply.github.com>
2020-11-28 12:04:36 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int a = Convert.ToInt32(Console.ReadLine());
+            string c = Console.ReadLine();
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (c == "+")
+            {
+                Console.WriteLine("=");
+                Console.WriteLine(a + b);
+                Console.ReadKey();
+            }
+            if (c == "-")
+            {
+                Console.WriteLine("=");
+                Console.WriteLine(a - b);
+                Console.ReadKey();
+            }
+           
+                   
+        }
+    }
+}
